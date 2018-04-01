bd1a9cb1a959bb023218c4135f0632e2d514dd30
prateek951 <prince.cenation@gmail.com>
2018-04-01 17:42:15 +0530

    extracted face and draw rect

--- FACE_RECOGNITION.py
+++ FACE_RECOGNITION.py
@@ -47,5 +47,26 @@ def knn(X_train,Y_train,xt,k=5):
 
 # @desc Face Detection Loop
 
+ret, frame = cam.read()
+if ret==True:
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray,1.3,5)
+    for (x,y,w,h) in faces:
+        # Extract the detected face
+        face = frame[y:y+h,x:x+w]
+        #Resize to a fixed shape
+        resized_face = cv2.resize(frame,(50,50)).flatten()
+        cv2.putText(frame,text,(x,y),font,1,(255,255,0),2)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+    cv2.imshow('Frame',frame)
 
+    if cv2.waitKey(1) == 27:
+        break
+    
+else:
+    print('Error')
+    break
 
+cam.release()
+cv2.destroyAllWindows()
